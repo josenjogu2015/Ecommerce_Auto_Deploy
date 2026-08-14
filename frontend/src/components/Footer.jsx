d629180a6b6f8cd0bd5b6c133f4a7257b81d9e33
joseph Nyaguthii <josenjogu2015@gmail.com>
2026-08-14 17:54:35 +0300

    Footer Update++

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
           target="_blank"
           className="text-blue-600 hover:underline"
         >
-          Rathan Kumar
+          Joseph Nyaguthii
         </a>
       </p>
 
