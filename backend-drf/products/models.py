da23b1654d8478fe6412bcc26e12f649644003c2
joseph Nyaguthii <josenjogu2015@gmail.com>
2026-08-15 15:22:15 +0300

    added slugs+

--- backend-drf/products/models.py
+++ backend-drf/products/models.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(max_length=255, unique=True, null=True, blank=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2) # 200.00
