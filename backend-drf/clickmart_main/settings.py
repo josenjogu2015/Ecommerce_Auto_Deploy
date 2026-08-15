caa438a7b5392136c68e2d54d5b293459c5a398f
joseph Nyaguthii <josenjogu2015@gmail.com>
2026-08-15 10:36:39 +0300

    Cors header allow domain slashes

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -174,6 +174,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
     'http://172.237.114.49:5173',
-    'http://store.josephnyaguthii.co.ke/',
-    'https://store.josephnyaguthii.co.ke/',
+    'http://store.josephnyaguthii.co.ke',
+    'https://store.josephnyaguthii.co.ke',
 ]
